4c41f894ee8722e95dd258bd3d250bee937e6b25
RT-VENOM <84263657+RT-VENOM@users.noreply.github.com>
2021-06-17 19:48:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,6 +62,11 @@ async def ban(ctx, member: discord.Member, *, reason = None):
         await ctx.channel.send("`You cannot ban yourself`")
         return
     await ctx.message.add_reaction('✅')
+    embedfordm=discord.Embed(
+        title="**BAN NOTICE**",
+        description=f"""{ member.mention } you are banned from { ctx.guild.name } by { ctx.author.mention } due to some unavoidable reasons which were created due to your activity in the server which are breaking the rules of { ctx.guild.name }.""",
+        color=0xd89522)
+    await ctx.member.send(embed = embedfordm)
     await member.ban(reason= reason)
     embed=discord.Embed(
         title="**MODERATION COMMAND**",
@@ -70,11 +75,7 @@ async def ban(ctx, member: discord.Member, *, reason = None):
     embed.set_thumbnail(url = 'https://media.discordapp.net/attachments/841947091659653162/854969004699156480/output-onlinegiftools.gif')
     embed.set_author(name = "BAN COMMAND EXECUTED", icon_url= "https://media.discordapp.net/attachments/841947091659653162/854969004699156480/output-onlinegiftools.gif")
     await ctx.send(embed = embed)
-    embedfordm=discord.Embed(
-        title="**BAN NOTICE**",
-        description=f"""{ member.mention } you are banned from { ctx.guild.name } by { ctx.author.mention } due to some unavoidable reasons which were created due to your activity in the server which are breaking the rules of { ctx.guild.name }.""",
-        color=0xd89522)
-    await ctx.member.send(embed = embedfordm)
+    
     
     
     
