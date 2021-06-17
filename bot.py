7d0e7917267012901bdfa9266abe5bf7640ff80c
RT-VENOM <84263657+RT-VENOM@users.noreply.github.com>
2021-06-17 19:17:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ client = commands.Bot(command_prefix='?')
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='?help'))
     print("bot is online")
 
 
