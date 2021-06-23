7fca62071b0edcec132fa6b5909c6835e59d4da8
RT-VENOM <84263657+RT-VENOM@users.noreply.github.com>
2021-06-23 14:42:12 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,47 @@
+import discord
+from discord import client
+from discord.ext import commands
+import config 
+from config import token
+import asyncio
+import os
+
+client = commands.Bot(command_prefix= '-')
+
+
+if __name__ == "__main__":
+    for file in os.listdir("./commands"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                client.load_extension(f"commands.{extension}")
+                print(f"Loaded extension '{extension}'")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(f"Failed to load extension {extension}\n{exception}")
+
+
+
+
+
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{len(client.guilds)} servers | -help'))
+    print("bot is online")
+
+
+
+@client.event
+async def on_guild_join():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{len(client.guilds)} servers | -help'))
+
+@client.event
+async def on_guild_remove():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{len(client.guilds)} servers | -help'))
+
+
+
+
+client.run(token)
