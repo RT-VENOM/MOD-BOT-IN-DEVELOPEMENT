3f73e322a2507b3ace92fd66de5e04ca426a62e8
RT-VENOM <84263657+RT-VENOM@users.noreply.github.com>
2021-06-28 15:12:14 +0530

    Update onguildjoin.py

--- commands/onguildjoin.py
+++ commands/onguildjoin.py
@@ -14,7 +14,7 @@ class Commands(commands.Cog, name="Commands"):
 
     @commands.Cog.listener()
     
-    async def on_guild_join(self, guild, ctx):
+    async def on_guild_join(self, ctx, guild):
         general = find(lambda x: x.name == 'general',  guild.text_channels)
         if not general:
             return
