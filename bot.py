c68da57ec086327de73253319683f0f51d728aed
RT-VENOM <84263657+RT-VENOM@users.noreply.github.com>
2021-06-30 21:43:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,11 +43,11 @@ async def on_guild_join(guild):
         Click on **[Support server!](https://discord.gg/4DSZ4skNgp)** to join the official `MOD BOT` support server.
                 """,
             color= 0xd89522)
-        embed.set_footer(text = "ENJOY USING ME IN YOUR SERVER")
-        embed.set_thumbnail(url = 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
-        embed.set_author(name = "MOD BOT", url='https://www.youtube.com/channel/UCt0g05Q4JTm1a5mxGj7JPQQ', icon_url= 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
+    embed.set_footer(text = "ENJOY USING ME IN YOUR SERVER")
+    embed.set_thumbnail(url = 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
+    embed.set_author(name = "MOD BOT", url='https://www.youtube.com/channel/UCt0g05Q4JTm1a5mxGj7JPQQ', icon_url= 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
         
-        await ctx.send(embed=embed)
+    await ctx.send(embed=embed)
 
 @client.event
 async def on_guild_remove(guild):
