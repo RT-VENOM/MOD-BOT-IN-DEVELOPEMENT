ce32c34a72add3b1d26ae215c53fd427d45e792f
RT-VENOM <84263657+RT-VENOM@users.noreply.github.com>
2021-06-30 21:20:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ async def on_guild_join(guild):
         embed.set_thumbnail(url = 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
         embed.set_author(name = "MOD BOT", url='https://www.youtube.com/channel/UCt0g05Q4JTm1a5mxGj7JPQQ', icon_url= 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
         
-        await ctx.author.send(embed=embed)
+        await ctx.send(embed=embed)
 
 @client.event
 async def on_guild_remove(guild):
