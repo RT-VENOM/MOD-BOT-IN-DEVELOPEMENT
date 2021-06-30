f1c2591fe0cb8070c32eff246a58bfb0b27b38f0
RT-VENOM <84263657+RT-VENOM@users.noreply.github.com>
2021-06-30 21:17:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,6 +36,18 @@ async def on_ready():
 @client.event
 async def on_guild_join(guild):
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{len(client.guilds)} servers | ?help'))
+    embed=discord.Embed(
+            title="**======== *THANK YOU!* ========**",
+            description=f"""Thanks for adding `MOD BOT` in your server
+                
+        Click on **[Support server!](https://discord.gg/4DSZ4skNgp)** to join the official `MOD BOT` support server.
+                """,
+            color= 0xd89522)
+        embed.set_footer(text = "ENJOY USING ME IN YOUR SERVER")
+        embed.set_thumbnail(url = 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
+        embed.set_author(name = "MOD BOT", url='https://www.youtube.com/channel/UCt0g05Q4JTm1a5mxGj7JPQQ', icon_url= 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
+        
+        await ctx.author.send(embed=embed)
 
 @client.event
 async def on_guild_remove(guild):
