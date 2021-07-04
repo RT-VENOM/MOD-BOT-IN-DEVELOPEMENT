c33bc79e62ce1e8d8ed28d83a4c2c51c14f8b7f9
RT-VENOM <84263657+RT-VENOM@users.noreply.github.com>
2021-07-04 13:56:55 +0530

    Update Invite.py

--- commands/Invite.py
+++ commands/Invite.py
@@ -15,23 +15,37 @@ class Invitation(commands.Cog, name="Invitation"):
 
     @commands.command(aliases = ['inv'], brief = '[Bot invite command]', description = 'This command is used to invite the bot in your server')
     async def invite(self, ctx):
-        await ctx.message.add_reaction('✅')
-        embed=discord.Embed(
-            title="**======== *WANNA ADD ME IN YOUR SERVER!* ========**",
-            description=f"""Do you liked using { self.bot.user.mention } and wanna add `MOD BOT` in your server
-                
-        Then click on **[Invite me!](https://discord.com/api/oauth2/authorize?client_id=853890206570119188&permissions=4294967287&scope=bot)** to add `MOD BOT` in your server.
-                """,
-            color= 0xd89522)
-        embed.set_footer(text = "ENJOY USING ME IN YOUR SERVER")
-        embed.set_thumbnail(url = 'https://media.discordapp.net/attachments/841947091659653162/854969004699156480/output-onlinegiftools.gif')
-        embed.set_author(name = "MOD BOT", url='https://www.youtube.com/channel/UCt0g05Q4JTm1a5mxGj7JPQQ', icon_url= 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
-        embed.set_image(url= 'https://media.discordapp.net/attachments/854545082634862622/854931871291670528/download_6_Fotor.jpg')
-        await ctx.author.send(embed=embed)
-        await asyncio.sleep(5)
-        await ctx.message.delete()
-    
+        try:
+            await ctx.message.add_reaction('✅')
+            embed=discord.Embed(
+                title="**======== *WANNA ADD ME IN YOUR SERVER!* ========**",
+                description=f"""Do you liked using { self.bot.user.mention } and wanna add `MOD BOT` in your server
 
+            Then click on **[Invite me!](https://discord.com/api/oauth2/authorize?client_id=853890206570119188&permissions=4294967287&scope=bot)** to add `MOD BOT` in your server.
+                    """,
+                color= 0xd89522)
+            embed.set_footer(text = "ENJOY USING ME IN YOUR SERVER")
+            embed.set_thumbnail(url = 'https://media.discordapp.net/attachments/841947091659653162/854969004699156480/output-onlinegiftools.gif')
+            embed.set_author(name = "MOD BOT", url='https://www.youtube.com/channel/UCt0g05Q4JTm1a5mxGj7JPQQ', icon_url= 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
+            embed.set_image(url= 'https://media.discordapp.net/attachments/854545082634862622/854931871291670528/download_6_Fotor.jpg')
+            await ctx.author.send(embed=embed)
+            await asyncio.sleep(5)
+            await ctx.message.delete()
+        except:
+            embed=discord.Embed(
+                title="**======== *WANNA ADD ME IN YOUR SERVER!* ========**",
+                description=f"""Do you liked using { self.bot.user.mention } and wanna add `MOD BOT` in your server
+
+            Then click on **[Invite me!](https://discord.com/api/oauth2/authorize?client_id=853890206570119188&permissions=4294967287&scope=bot)** to add `MOD BOT` in your server.
+                    """,
+                color= 0xd89522)
+            embed.set_footer(text = "ENJOY USING ME IN YOUR SERVER")
+            embed.set_thumbnail(url = 'https://media.discordapp.net/attachments/841947091659653162/854969004699156480/output-onlinegiftools.gif')
+            embed.set_author(name = "MOD BOT", url='https://www.youtube.com/channel/UCt0g05Q4JTm1a5mxGj7JPQQ', icon_url= 'https://cdn.discordapp.com/attachments/845324641521107004/845391299196747796/3dgifmaker81594_1.gif')
+            embed.set_image(url= 'https://media.discordapp.net/attachments/854545082634862622/854931871291670528/download_6_Fotor.jpg')
+            await ctx.author.send(embed=embed)
+            await asyncio.sleep(5)
+            await ctx.message.delete()
 
 # your code here
 
